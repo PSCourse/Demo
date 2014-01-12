2f12f86c45e2ebee04668847509416c1d09202ff
PSCourse <id.personaltraining@gmail.com>
2014-01-12 05:38:12 +0100

    new try

--- Demo.Tests/Class1.cs
+++ Demo.Tests/Class1.cs
@@ -1,7 +1,9 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,73 @@ namespace Demo.Tests
         public void AddTest()
         {
             Assert.IsTrue(true);
+        }
+
+    }
+
+    [TestFixture]
+    public class UtilityTest
+    {
+        [Test]
+        public void FindConflictingReferences()
+        {
+            var assemblies = GetAllAssemblies(@"C:\Users\IrisDaniela\Documents\GitHub\Demo\Demo\bin");
+
+            var references = GetReferencesFromAllAssemblies(assemblies);
+
+            var groupsOfConflicts = FindReferencesWithTheSameShortNameButDiffererntFullNames(references);
+
+            foreach (var group in groupsOfConflicts)
+            {
+                Console.Out.WriteLine("Possible conflicts for {0}:", group.Key);
+                foreach (var reference in group)
+                {
+                    Console.Out.WriteLine("{0} references {1}",
+                                          reference.Assembly.Name.PadRight(25),
+                                          reference.ReferencedAssembly.FullName);
+                }
+            }
+        }
+
+        private IEnumerable<IGrouping<string, Reference>> FindReferencesWithTheSameShortNameButDiffererntFullNames(List<Reference> references)
+        {
+            return from reference in references
+                   group reference by reference.ReferencedAssembly.Name
+                       into referenceGroup
+                       where referenceGroup.ToList().Select(reference => reference.ReferencedAssembly.FullName).Distinct().Count() > 1
+                       select referenceGroup;
+        }
+
+        private List<Reference> GetReferencesFromAllAssemblies(List<Assembly> assemblies)
+        {
+            var references = new List<Reference>();
+            foreach (var assembly in assemblies)
+            {
+                foreach (var referencedAssembly in assembly.GetReferencedAssemblies())
+                {
+                    references.Add(new Reference
+                    {
+                        Assembly = assembly.GetName(),
+                        ReferencedAssembly = referencedAssembly
+                    });
+                }
+            }
+            return references;
+        }
+
+        private List<Assembly> GetAllAssemblies(string path)
+        {
+            var files = new List<FileInfo>();
+            var directoryToSearch = new DirectoryInfo(path);
+            files.AddRange(directoryToSearch.GetFiles("*.dll", SearchOption.AllDirectories));
+            files.AddRange(directoryToSearch.GetFiles("*.exe", SearchOption.AllDirectories));
+            return files.ConvertAll(file => Assembly.LoadFile(file.FullName));
+        }
+
+        private class Reference
+        {
+            public AssemblyName Assembly { get; set; }
+            public AssemblyName ReferencedAssembly { get; set; }
         }
 
     }

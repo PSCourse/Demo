e5b3151f022cc47bdc2911d72009895d3354d5f0
PSCourse <id.personaltraining@gmail.com>
2014-01-12 03:27:19 +0100

    Using NUnit now

---
+++ Demo.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
